1033127119e37fda8eba036cbd1c3883fde7779e
Lawrence E. Bakst <leb@me.com>
2014-04-28 08:50:16 -0400

    small change to build shell script to be more verbose

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+echo "make gensort"
 cd gensort
 make
 cd ..
+echo "generate datasets"
 cd datasets
 ./gendata.sh
 cd ..
+echo "build Go Heapsort"
 go build -v
 cd hsort
 go build -v
+echo "build C Heapsort"
 cd ../c
 make
